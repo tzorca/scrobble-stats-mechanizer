550a1e83af464e4c83e9767638f1a6c42a8481f2
tzorca <tzorca@users.noreply.github.com>
2016-08-24 18:12:28 -0400

    Add previously unversioned files.

---
+++ TagMarkerForScrobblers/utilities/MiscExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagMarkerForScrobblers
+{
+    public static class MiscExtensions
+    {
+        public static string FormatAs_yyyyMMdd(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyyMMdd");
+        }
+        public static List<string> SplitIntoLines(this string input)
+        {
+            return (input ?? "")
+                .Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None)
+                .ToList();
+        }
+    }
+}
